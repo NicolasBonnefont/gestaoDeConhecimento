d40cde62e95c179049839bf5f2a6b387e799b714
Nicolas <nicolas.7l@hotmail.com>
2020-07-01 16:41:30 -0300

    acerto

--- public/js/cadastro/index.js
+++ public/js/cadastro/index.js
@@ -75,7 +75,7 @@ async function igualaUsuario() {
       .then(function (response) {
         document.getElementById('fieldset').disabled = false
         document.getElementById('usuarioAltera').value = response.data.Usuario
-        document.getElementById('imgNovo').src = response.data.ur
+        document.getElementById('imgAltera').src = response.data.ur
 
       })
       .catch(function (erro) {
