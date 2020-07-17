db3076ff36d43fa97bc0e53644da7c0556a4348e
Nicolas <nicolas.7l@hotmail.com>
2020-07-17 11:22:23 -0300

    acertos

--- public/js/pesquisa/pesquisa.js
+++ public/js/pesquisa/pesquisa.js
@@ -23,7 +23,7 @@ async function carregaPesquisa() {
     .then(function (response) {
       if (response.data.length > 0) {
         response.data.map(topico =>
-          lista += `<div onclick="location.href='/detalhe?id=${topico.id}'" class="row card cardCustom cardSubTopico">
+          lista += `<div onclick="location.href='/detalhe?id=${topico.id}'" class="text-center row card cardCustom cardSubTopico">
          <h4 class="col">${topico.Titulo} </h4>
          <p class="col card-text"><small class="text-muted">Ultima atualização: ${topico.updated_at}</small></p>
          <p class="col card-text"><small class="text-muted">Alterado por: ${topico.UsuarioAlteracao ? topico.UsuarioAlteracao : topico.Usuario}</small>
