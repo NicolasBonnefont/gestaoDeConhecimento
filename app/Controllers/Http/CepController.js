ed02db846adab7f7a2a7fe4c17e9f48c3fdc0d62
Nicolas <nicolas.7l@hotmail.com>
2020-07-27 09:05:49 -0300

    acerto

--- app/Controllers/Http/CepController.js
+++ app/Controllers/Http/CepController.js
@@ -2,10 +2,8 @@
 
 const { default: Axios } = require("axios")
 const axiosRateLimit = require("axios-rate-limit")
-
 const Cep = use('App/Models/Cep')
 const Database = use('Database')
-
 
 class CepController {
 
@@ -13,17 +11,14 @@ class CepController {
 
     const data = await Cep.all()
 
-
-
     return data
 
   }
 
   async atualizaCEP({ request, response, params }) {
-    const http = axiosRateLimit(Axios.create(), { maxRequests: 1, perMilliseconds: 5000})
+    const http = axiosRateLimit(Axios.create(), { maxRequests: 1, perMilliseconds: 5000 })
 
     let data = await Database.select('*').from('ceps')
-
 
     async function atualizaTabela(cep, longitude, latitude) {
       await Database
@@ -37,29 +32,27 @@ class CepController {
 
     try {
       data.map(async item =>
-        item.LATITUDE == null || item.LONGITUDE== null || item.LATITUDE == '' || item.LONGITUDE== '' ?
-        await http.get(`https://www.cepaberto.com/api/v3/cep?cep=${item.CEP}`,{
-          headers: {
-            'Authorization': 'Token token=677ae19e734a21bfbffec9bc0ea849a9'
-          }
-        })
-          .then(function (response) {
-
-            console.log(item.CEP,response.data.longitude, response.data.latitude )
-            if(response.data.cep){
-              atualizaTabela(item.CEP, response.data.longitude, response.data.latitude)
+        item.LATITUDE == null || item.LONGITUDE == null || item.LATITUDE == '' || item.LONGITUDE == '' ?
+          await http.get(`https://www.cepaberto.com/api/v3/cep?cep=${item.CEP}`, {
+            headers: {
+              'Authorization': 'Token token=677ae19e734a21bfbffec9bc0ea849a9'
             }
-
-
           })
-          .catch(function (err) {
-            console.log(err)
-          })
+            .then(function (response) {
+
+              console.log(item.CEP, response.data.longitude, response.data.latitude)
+              if (response.data.cep) {
+                atualizaTabela(item.CEP, response.data.longitude, response.data.latitude)
+              }
+
+            })
+            .catch(function (err) {
+              console.log(err)
+            })
           : false
       )
 
       return response.status(200).send({ mensagem: 'Tabela Atualizada ! ' })
-
 
     } catch (err) {
       return response.status(500).send({ Erro: 'Ocorreu um erro para atualizar a tabela ! ' }, err)
