0007169951fd82889d47cc304df023a583da4b70
Nicolas <nicolas.7l@hotmail.com>
2020-08-06 14:10:42 -0300

    acerto

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -49,7 +49,7 @@ class FileController {
 
    // return response.download(Helpers.publicPath(`uploads/${file.file}`))
 
-    return response.download(file.file)
+    return Helpers.tmpPath(file.file)
 
 
 
